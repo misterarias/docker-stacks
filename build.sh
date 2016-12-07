b85b14edc79c0622cf2920b03a8b404892663a32
Juan Arias <juan.arias@bluetab.net>
2016-12-07 13:44:52 +0100

    Smarter build script

--- build.sh
+++ build.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 RUN_ALL=""
-for nb in $(echo base-notebook minimal-notebook scipy-notebook pyspark-notebook all-spark-notebook) ; do
-  [ -z "$RUN_ALL" ] && printf "\nBuilding: %s, proceed?? [s/n/A]\n" "${nb}" && read "key"
-  [ "$key" == "A" ] && RUN_ALL="yes"
-  [ "$key" == "s" -o ! -z "$RUN_ALL" ] && docker build -t "$nb" "${nb}/"
+for nb in $(echo base-notebook minimal-notebook scipy-notebook pyspark-notebook all-spark-notebook my-custom-notebook) ; do
+  [ -z "$RUN_ALL" ] && printf "\nBuilding: %s, proceed?? [(y)es / (n)o / (a)ll / (q)uit]\n" "${nb}" && read "key" 2>&1 >/dev/null
+  case "$key" in
+    [aA]  ) RUN_ALL="yes" ;;
+    [nN]  ) continue ;;
+    [qQ]  ) exit -1 ;;
+    *  ) ;;
+  esac
+  docker build -t "$nb" "${nb}/" || exit 1
 done
